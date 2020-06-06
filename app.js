6541dc9c5004b76b4f0e3f4e1e29f5839da349d2
ashminbhandari <abhandar@ramapo.edu>
2020-06-06 17:42:50 -0400

    process.env.PORT instead

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+server.listen(process.env.PORT || 3000);
 
 //Directories
 app.use(express.static(__dirname + '/public'));
@@ -13,8 +14,6 @@ app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
-
-server.listen(process.env.PORT || 3000);
 
 io.sockets.on('connection', function(socket) {
 
